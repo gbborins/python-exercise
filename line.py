f5908dc23a00b7d2ba5889319976643c730bca28
Gabriel Bortolin <gabreilbortolin2007@gmail.com>
2025-10-14 09:35:34 -0300

    Atualizar o line.py

--- line.py
+++ line.py
@@ -3,5 +3,17 @@ line = list(range(1,last+1))
 while True:
     print(f"{len(line)} clientes restantes")
     print(f"fila atual: {line}")
-    choice = str(input("Digite C(adicionar cliente), A(atendimento), F(fechar)))
-
+    choice = str(input("Digite C(adicionar cliente), A(atendimento), F(fechar)"))
+    if choice.lower() == "a":
+        if len(line) > 0:
+            aten = line.pop(0)
+            print(f"Cliente {aten} atendido}
+        else:
+            print(f"Não há ninguém")
+    elif choice.lower() == "c":
+        ultimo += 1
+        fila.append(ultimo)
+    elif choice.lower() == "f"
+        break
+    else
+        print("Operacao inválida")
