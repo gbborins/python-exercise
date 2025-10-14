2337aed27414bd70357fba52fd84f866af1d2ec8
Gabriel Bortolin <gabreilbortolin2007@gmail.com>
2025-10-14 08:53:46 -0300

    Atualizar o line.py

--- line.py
+++ line.py
@@ -1,1 +1,7 @@
-a
+last = int(input(f"quantidade inicial"))
+line = list(range(1,last+1))
+while True:
+    print(f"{len(line)} clientes restantes")
+    print(f"fila atual: {line}")
+    choice = str(input("Digite C(adicionar cliente), A(atendimento), F(fechar)))
+
